7c947d4bd4295929a5aaa5be6b89ef2d96690bed
Washington Irving <iamwashingyou@gmail.com>
2021-07-26 21:45:42 +0300

    add comments

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
+// Pass servers location
 const socket = io('http://localhost:3000');
 
+// Get DOM-elements
 const messageContainer = document.getElementById('message-container');
 const messageForm = document.getElementById('send-container');
 const messageInput = document.getElementById('message-input');
 
+// Get user's name
 const name = prompt('Enter your name');
+// Notify that user's joined
 appendMessage(`${name} (you) joined`);
+// Send user's name to the server
 socket.emit('new-user', name);
 
+// Get another user's name
 socket.on('user-connected', (name) => {
   appendMessage(`${name} joined`);
 });
 
+// Notify a user's disconnected
 socket.on('user-disconnected', (name) => {
   appendMessage(`${name} left`);
 });
 
+// Get another user's message
 socket.on('chat-message', (data) => {
   appendMessage(`${data.name}: ${data.message}`);
 });
 
+// Send message to the server and show on user's screen
 messageForm.addEventListener('submit', (event) => {
   event.preventDefault();
   const message = messageInput.value;
